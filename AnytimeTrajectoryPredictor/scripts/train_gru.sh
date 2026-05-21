7b579c4c3190a4be4e26b2d70b1d02ac24e67780
Léonard Amsler <leonard.amsler@epfl.ch>
2026-05-21 16:51:26 +0200

    Training script gru

---
+++ AnytimeTrajectoryPredictor/scripts/train_gru.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#SBATCH --job-name=traj_gnn
+#SBATCH --time=10:00:00
+#SBATCH --account=cs-503
+#SBATCH --qos=cs-503
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
+#SBATCH --mem=64G
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --output=logs/%x_%j.out
+#SBATCH --error=logs/%x_%j.err
+
+# ---------------------------
+# conda setup
+# ---------------------------
+source ~/miniforge3/etc/profile.d/conda.sh
+conda activate vtp
+
+# ---------------------------
+# move to project directory
+# ---------------------------
+cd /home/muralikr/VisualThinkingProject/AnytimeTrajectoryPredictor
+
+echo "Running on: $(hostname)"
+echo "Python: $(which python)"
+
+# ---------------------------
+# run training
+# ---------------------------
+python scripts/train_trajectory_model.py \
+  --config /home/muralikr/VisualThinkingProject/configs/gru_config.yaml
